2fd457027f24680b516f1f63a132b5c739851a37
patrick.elano.melo <patrick.elano.melo@outlook.com>
2018-06-07 09:41:51 -0400

    UPDATE TEST CONTENT

--- test.py
+++ test.py
@@ -19,7 +19,7 @@ class Test(unittest.TestCase):
         result = self.app.get('/') 
 
         # verifica o retorno do conteudo da pagina
-        self.assertRegex(result.data.decode(), "BNB PIPELINE DEVOPS INFRAESTRUTURA_v1")
+        self.assertRegex(result.data.decode(), "BNB PIPELINE DEVOPS INFRAESTRUTURA_v2")
 
 
 if __name__ == "__main__":
